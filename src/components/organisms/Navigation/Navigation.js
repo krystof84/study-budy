075823d5c39cc24f43f4afcce61b0617af6680d2
Czajka <krzysztof.czajka@forum-media.pl>
2021-12-22 14:27:54 +0100

    Mocked auth

--- src/components/organisms/Navigation/Navigation.js
+++ src/components/organisms/Navigation/Navigation.js
@@ -16,6 +16,9 @@ const Navigation = () => {
         </h1>
       </Logo>
       <StyledLink to="/group">Dashboard</StyledLink>
+      <StyledLink as="a" onClick={() => localStorage.removeItem('token')}>
+        Login
+      </StyledLink>
     </Wrapper>
   );
 };
